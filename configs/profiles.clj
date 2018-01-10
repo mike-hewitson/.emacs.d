895560afccce66c1814db9228825a18fb51a5ace
mikeyjcat <mike@hewitson.co.za>
2018-01-10 06:07:51 +0200

    feat: added config files

---
+++ configs/profiles.clj
@@ -0,0 +1,9 @@
+{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]}
+ :user {:plugins [[lein-midje "3.2.1"]
+                  [lein-bikeshed "0.5.0"]
+                  [lein-kibit "0.1.5"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-cljfmt "0.5.7"]
+                  [cider/cider-nrepl "0.15.1"]
+                  [refactor-nrepl "2.3.1"]]}}
+
