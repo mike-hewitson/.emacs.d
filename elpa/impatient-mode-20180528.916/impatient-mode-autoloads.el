0d9b5d5bc3a67f85035b103fb54a5cc03d0ded21
Mike Hewitson <mike@hewitson.co.za>
2018-09-04 07:10:54 +0200

    update packages

--- elpa/impatient-mode-20180528.916/impatient-mode-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; impatient-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "impatient-mode" "impatient-mode.el" (23372
-;;;;;;  8550 396135 88000))
-;;; Generated autoloads from impatient-mode.el
-
-(autoload 'impatient-mode "impatient-mode" "\
-Serves the buffer live over HTTP.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("impatient-mode-pkg.el") (23372 8550 391263
-;;;;;;  645000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; impatient-mode-autoloads.el ends here
